fe0fc64b33066f75f77a35286a3eb0064aabfe85
Steven Lattenhauer II <gunplagamer@gmail.com>
2024-09-30 15:58:17 -0400

    Initial Commit

---
+++ The Chosen SparkTests/The_Chosen_SparkTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_Chosen_SparkTests.swift
+//  The Chosen SparkTests
+//
+//  Created by Steven Lattenhauer 2nd on 9/30/24.
+//
+
+import Testing
+@testable import The_Chosen_Spark
+
+struct The_Chosen_SparkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
